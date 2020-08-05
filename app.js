19c91ff4f2cc47b93e2efaa8bf23c3328768d6ce
Ionhedblom <ionysoshedblom@gmail.com>
2020-08-05 17:38:30 +0200

    Startgame function now works

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', function () {
+    var bird = document.querySelector('.bird');
+    var gameDisplay = document.querySelector('.game-container');
+    var ground = document.querySelector('.ground');
+    var birdLeft = 220;
+    var birdBottom = 100;
+    function startGame() {
+        bird.style.bottom = birdBottom + 'px';
+    }
+    startGame();
+});
--- app.ts
+++ app.ts
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const bird = document.querySelector('.bird');
+  const bird = document.querySelector('.bird') as HTMLElement;
+  const gameDisplay = document.querySelector('.game-container');
+  const ground = document.querySelector('.ground');
+
+  let birdLeft: number = 220;
+  let birdBottom: number = 100;
+
+  function startGame() {
+    bird.style.bottom = birdBottom + 'px';
+  }
+
+  startGame();
 });
